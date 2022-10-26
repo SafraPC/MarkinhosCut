8d715f47561e746bfe3181f8cefb7cca0e200f67
= <leandrosafra.jconectada@gmail.com>
2022-10-26 19:57:22 -0300

    create professional component

--- src/main/java/com/example/marquinhoscut/Components/ProfessionalField.java
+++ src/main/java/com/example/marquinhoscut/Components/ProfessionalField.java
@@ -1,4 +1,33 @@
 package com.example.marquinhoscut.Components;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
 
 public class ProfessionalField {
+	
+	@FXML
+	private AnchorPane anchorId;
+	
+	@FXML
+	private TextField qtdField;
+	
+	@FXML
+	private TextField qtdField1;
+	
+	@FXML
+	void handleDelete(ActionEvent event) {
+	
+	}
+	
+	@FXML
+	void handleEdit(ActionEvent event) {
+	
+	}
 }
--- src/main/java/com/example/marquinhoscut/Components/ServiceField.java
+++ src/main/java/com/example/marquinhoscut/Components/ServiceField.java
@@ -1,5 +1,28 @@
 package com.example.marquinhoscut.Components;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class ServiceField {
+	@FXML
+	private AnchorPane anchorId;
+	
+	@FXML
+	private TextField qtdField;
+	
+	@FXML
+	private TextField qtdField1;
+	
+	@FXML
+	void handleDelete(ActionEvent event) {
+	
+	}
+	
+	@FXML
+	void handleEdit(ActionEvent event) {
+	
+	}
 
 }
