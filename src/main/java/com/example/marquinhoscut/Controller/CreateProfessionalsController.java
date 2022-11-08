b6e3d0e718e41958c9e267f3b63b5d5b57bc0e78
SafraPC <leandrosafra.jconectada@gmail.com>
2022-11-08 00:16:25 -0300

    feature: create handle action in those new screns

--- src/main/java/com/example/marquinhoscut/Controller/CreateProfessionalsController.java
+++ src/main/java/com/example/marquinhoscut/Controller/CreateProfessionalsController.java
@@ -1,18 +1,28 @@
 package com.example.marquinhoscut.Controller;
 
 import com.example.marquinhoscut.Utils.Bar.AdminBar;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CreateProfessionalsController extends AdminBar {
     @FXML
     private Button exitButton,servicesButton,professionalButton,resultsButton,back;
 
     @FXML
+    private TextField nameField,cpfField;
+    @FXML
     void initialize(){
         handleNavigationBar(exitButton,servicesButton,professionalButton,resultsButton);
         back.setOnAction(event->{
             goTo(back,"professionals.fxml","Profissionais");
         });
+    }
+
+    @FXML
+    public void handleCreateProfessional(ActionEvent actionEvent) {
+        System.out.println(nameField.getText());
+        System.out.println(cpfField.getText());
     }
 }
--- src/main/java/com/example/marquinhoscut/Controller/CreateServicesController.java
+++ src/main/java/com/example/marquinhoscut/Controller/CreateServicesController.java
@@ -1,12 +1,17 @@
 package com.example.marquinhoscut.Controller;
 
 import com.example.marquinhoscut.Utils.Bar.AdminBar;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class CreateServicesController  extends AdminBar {
     @FXML
     private Button exitButton,servicesButton,professionalButton,resultsButton,back;
+
+    @FXML
+    private TextField nameField,priceField;
 
     @FXML
     void initialize(){
@@ -14,5 +19,11 @@ public class CreateServicesController  extends AdminBar {
         back.setOnAction(event->{
             goTo(back,"services.fxml","Serviços");
         });
+    }
+
+    @FXML
+    public void handleCreateNewService(ActionEvent actionEvent) {
+        System.out.println(nameField.getText());
+        System.out.println(priceField.getText());
     }
 }
