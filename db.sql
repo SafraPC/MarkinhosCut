bdba40d42b32aaa0698efe2ad71331a301738e78
Marcelo dos santos almeida <marcelo1304@gmail.com>
2022-11-09 17:42:47 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -30,7 +30,7 @@ serviceName VARCHAR (100) NOT NULL,
 isActive BOOLEAN NOT NULL
 );
 
-CREATE TABLE qtdSelling(
+CREATE TABLE qtdService(
 serviceId INTEGER NOT NULL,
 sellingId INTEGER NOT NULL,
 quantity INTEGER NOT NULL,
@@ -39,7 +39,7 @@ FOREIGN KEY (sellingId) REFERENCES Selling (sellingId)
 ON DELETE CASCADE
 );
 
-INSERT INTO Professional (cpf, professionalName, isActive) VALUES 
+INSERT INTO Professional (cpf, professionalName, isActive) VALUES
 ("48663164890", "Marcos", TRUE),
 ("55698877455", "Henrique", TRUE),
 ("77588933641", "Marcelo", TRUE),
@@ -48,43 +48,43 @@ INSERT INTO Professional (cpf, professionalName, isActive) VALUES
 ("22046678988", "joao", FALSE),
 ("11447755588", "agata", FALSE);
 
-INSERT INTO Service (price, serviceName, isActive) VALUES 
+INSERT INTO Service (price, serviceName, isActive) VALUES
 (25.0, "Corte Degrade", TRUE),
 (10.0, "Sobrancelha", TRUE),
 (20.0, "Barba", TRUE),
 (35.0, "Mechas", FALSE),
 (50.0, "Progressiva", TRUE);
 
-INSERT INTO PaymentMethod (paymentName) values 
+INSERT INTO PaymentMethod (paymentName) values
 ("Pix");
 
-INSERT INTO Selling (cpf, paymentName, total, sellingDate) values 
+INSERT INTO Selling (cpf, paymentName, total, sellingDate) values
 ("48663164890", "Pix", 0, '2020-03-01');
 
-insert into qtdSelling ( sellingId, serviceId,quantity) values 
+insert into qtdService ( sellingId, serviceId,quantity) values
 (1, 1, 2),
 (1, 2, 3);
 
-DELIMITER | 
+DELIMITER |
 CREATE PROCEDURE createProfessional (nameParam VARCHAR(100), cpfParam VARCHAR(15))
        BEGIN
-		   INSERT INTO Professional (cpf, professionalName, isActive) VALUES 
+		   INSERT INTO Professional (cpf, professionalName, isActive) VALUES
 		  (cpfParam, nameParam, TRUE);
 		END |
 
 CALL createProfessional("Safrudo","42413049872");
 
-DELIMITER | 
+DELIMITER |
 CREATE PROCEDURE editProfessional (nomeParam VARCHAR(100), cpfParam VARCHAR(15), professionalIdParam INTEGER)
        BEGIN
          UPDATE Professional SET professionalName = nomeParam, cpf = cpfParam WHERE professionalId = professionalIdParam;
 		END |
-        
+
 CALL editProfessional("Henrique123","55698877455123123123", 2);
 CALL editProfessional('Henriquee', 5569887745566,2);
 SELECT * FROM Professional;
-        
-DELIMITER | 
+
+DELIMITER |
 CREATE PROCEDURE changeProfessionalStatus (changeTo BOOLEAN,professionalIdParam INTEGER)
        BEGIN
          UPDATE Professional SET isActive = changeTo WHERE professionalId = professionalIdParam;
@@ -93,6 +93,5 @@ CREATE PROCEDURE changeProfessionalStatus (changeTo BOOLEAN,professionalIdParam 
 CALL changeProfessionalStatus(FALSE, 8);
 
 
-        
 
 
