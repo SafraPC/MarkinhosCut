676ca3f86d4112213e701370d65bc4b4ac908b6d
Safra <leandrosafra.jconectada@gmail.com>
2022-11-20 23:09:40 -0300

    feature: finish deatils view

---
+++ src/main/java/com/example/marquinhoscut/Model/SellingDetailed.java
@@ -0,0 +1,63 @@
+package com.example.marquinhoscut.Model;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.util.Date;
+
+public class SellingDetailed {
+    private int sellingId,quantity;
+    private String professional,paymentName,serviceName;
+    private Double total,productPrice;
+    private Date sellingDate;
+
+    public SellingDetailed(int sellingId, String professional, String paymentName, Double total, Date sellingDate) {
+        this.sellingId = new SimpleIntegerProperty(sellingId).get();
+        this.professional = new SimpleStringProperty(professional).get();
+        this.paymentName = new SimpleStringProperty(paymentName).get();
+        this.total = new SimpleDoubleProperty(total).get();
+        this.sellingDate = sellingDate;
+    }
+
+    public SellingDetailed(int sellingId, String professional, String paymentName, Date sellingDate,
+                           String serviceName, Double productPrice, int quantity) {
+        this.sellingId = new SimpleIntegerProperty(sellingId).get();
+        this.professional = new SimpleStringProperty(professional).get();
+        this.paymentName = new SimpleStringProperty(paymentName).get();
+        this.total = new SimpleDoubleProperty(total).get();
+        this.sellingDate = sellingDate;
+        this.serviceName = new SimpleStringProperty(serviceName).get();
+        this.productPrice = new SimpleDoubleProperty(productPrice).get();
+        this.quantity = new SimpleIntegerProperty(quantity).get();
+    }
+    
+    public int getSellingId(){
+        return sellingId;
+    }
+    public String getProfessional(){
+        return professional;
+    }
+    public String getpaymentName(){
+        return paymentName;
+    }
+    public Double getTotal(){
+        return total;
+    }
+
+    public String getSellingDate() {
+        return sellingDate.toString();
+    }
+    
+    public String getServiceName() {
+        return serviceName;
+    }
+    
+    public Double getProductPrice() {
+        return productPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
